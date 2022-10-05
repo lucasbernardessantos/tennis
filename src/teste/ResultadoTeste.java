01d58f44238019d4ab4f803b9a50fdd02e6d3305
Aluno do Unilasalle <aluno.unilasalle@acad.nit>
2022-10-04 21:05:39 -0300

    Novos testes add

--- src/teste/ResultadoTeste.java
+++ src/teste/ResultadoTeste.java
@@ -8,9 +8,51 @@ import control.Resultado;
 
 public class ResultadoTeste {
 	@Test
-	public void teste_resultado() {
+	public void teste_resultado_1() {
 		Resultado resultado = new Resultado();
 		String[] jogos = {"v","v","v","v","v","v"};
 		Assert.assertEquals(1, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_2() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"v","v","v","v","v","p"};
+		Assert.assertEquals(1, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_3() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"v","v","v","v","p","p"};
+		Assert.assertEquals(2, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_4() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"v","v","v","p","p","p"};
+		Assert.assertEquals(2, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_5() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"v","v","p","p","p","p"};
+		Assert.assertEquals(3, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_6() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"v","p","p","p","p","p"};
+		Assert.assertEquals(3, resultado.resultado(jogos), 0);
+	}
+	
+	@Test
+	public void teste_resultado_7() {
+		Resultado resultado = new Resultado();
+		String[] jogos = {"p","p","p","p","p","p"};
+		Assert.assertEquals(-1, resultado.resultado(jogos), 0);
 	}
 }
